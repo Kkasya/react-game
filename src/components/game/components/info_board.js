c9fc9db19a3ac0b6a9b96f5b43b36cfd1fc53b92
Kkasya <kasya--k@yandex.by>
2021-02-25 11:10:03 +0300

    feat: add timer

--- src/components/game/components/info_board.js
+++ src/components/game/components/info_board.js
@@ -1,16 +1,40 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import Context from "./context";
 
 import '../game.css'
 
 const InfoBoard = () => {
   const [contextMove, setContext] = useContext(Context);
+  const [timer, setTimer] =useState(0);
+
+  useEffect(() => {
+    let counter;
+    if (timer >= 0) {
+      counter = setTimeout(() => setTimer((c) => c + 1), 1000)
+    }
+
+    return () => {
+      if (counter) {
+        clearTimeout(counter);
+      }
+    };
+  }, [timer]);
+
+  const padTime = (time) => {
+    return String(time).length === 1 ? `0${time}` : `${time}`;
+  };
+
+  const format = (time) => {
+    const min = Math.floor(time / 60);
+    const sec = time % 60;
+    return `${padTime(min)}:${padTime(sec)}`;
+  }
 
   return (
     <div className="d-flex justify-content-between text-info">
       <div className="d-flex info-item">
         <span>Time:</span>
-        <span id='time' className='text-success'>00:00:00</span>
+        <span id='time' className='text-success'>{format(timer)}</span>
       </div>
       <div className="d-flex info-item">
         <span id='moves' className='text-success'>{contextMove}</span>
