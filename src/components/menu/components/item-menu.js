77c5a7e8b033f05095a298acf5456abbbefe754d
Anastassia Kozlovskaya <kasya--k@yandex.ru>
2021-03-03 14:52:28 +0300

    fix: large ru letters

--- src/components/menu/components/item-menu.js
+++ src/components/menu/components/item-menu.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import {connect} from "react-redux";
 
 import '../menu.css';
 import '../../app/App.css'
 
-const ItemMenu = ({title, operation}) => {
+const ItemMenu = ({title, operation, lang}) => {
+  console.log(lang);
+  const fontSize = {
+    fontSize: (lang === 'en') ? '2rem' : '1.5rem',
+  };
   return (
-      <button className="btn btn-success btn-lg btnShadow"
+      <button className="btn btn-success btn-lg btnShadow"  style={fontSize}
               onClick={operation}>
         {title}
       </button>
   )
 };
-    export default ItemMenu;
+
+const mapStateToProps = (state) => ({
+  lang: state.lang,
+});
+
+export default connect(mapStateToProps)(ItemMenu);
